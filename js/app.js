519c84f86c96c6932a8b667f5bc56a61ca118b30
tams1993 <tams1993@gmail.com>
2016-02-07 00:40:13 +0700

    update

--- js/app.js
+++ js/app.js
@@ -1,16 +1,49 @@
 /**
  * Created by MR.T on 1/7/2016.
  */
-var myApp = angular.module('myApp', []);
+jQuery.scrollSpeed(100, 800);
 
-myApp.controller('mainController',['$scope','$filter',function($scope,$filter){
+$("#css3-animated-example").collapse({
+    accordion: true,
+    open: function() {
+    this.addClass("open");
+    this.css({ height: this.children().outerHeight() });
+    },
+    close: function() {
+    this.css({ height: "0px" });
+    this.removeClass("open");
+    }
+    });
 
-    $scope.handle = '';
-    $scope.lowercasehandle = function() {
+//$('.expandme').expandme();
 
-        return $filter('lowercase')($scope.handle);
+$(document).ready(function(){
 
-    };
+    $('#image').hide();
+    $('.expandme').expandme({
+        onBeforeExpand: function(settings){
+
+            $('#image').fadeIn();
 
 
-}]);
+        },
+
+        onBeforeCollapse: function(){
+
+            $('#image').fadeOut();
+
+        }
+    });
+});
+
+/*
+
+$(document).ready(function(){
+    $('.expandme').expandme({
+        onBeforeCollapse: function(settings){
+            var articleTop = $(this).parents('.article').offset().top;
+            $('body').animate({scrollTop: articleTop}, settings.speed);
+        }
+    });
+});
+*/
